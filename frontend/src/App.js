7e5f6dd599631ea2971167f13043073dbf38f250
Your Name <harrison@goloansnap.com>
2022-02-18 18:47:19 -0700

    more frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Button } from 'react-bootstrap';
+import metamaskLogo from './assets/images/metamask.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="secondary">
+          <img src={metamaskLogo}/>
+        </Button>  
         <p>Hello world</p>
       </header>
     </div>
