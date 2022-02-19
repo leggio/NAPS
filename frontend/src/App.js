55e133a07b688afb4264212c583ba524200404fa
Zane Witherspoon <zane.witherspoon@gmail.com>
2022-02-19 12:59:50 -0700

    pages for naps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,53 +6,57 @@ import {
   Route
 } from "react-router-dom";
 import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 
 import pyramid from './assets/images/Pyramid.png'
 
 import MarketplacePage from './components/marketplacePage.js'
+import Nap from './components/nap.js'
+import Dao from './components/dao.js'
 
 function App() {
-  const [page, setPage] = useState("marketplace");
-
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="logo-container">
-          <img src={pyramid} alt="logo" className="logo"/>
-          <div>
-            <div>NAPS</div>
-            <div className="small-text">Not a pyramid scheme</div>
+      <BrowserRouter>
+        <header className="App-header">
+          <div className="logo-container">
+            <img src={pyramid} alt="logo" className="logo"/>
+            <div>
+              <div>NAPS</div>
+              <div className="small-text">Not a pyramid scheme</div>
+            </div>
           </div>
+          <menu className="menu">
+            <Nav
+              activeKey="/Marketplace"
+            >
+              <Nav.Item>
+                <LinkContainer to={`/Marketplace`}>
+                  <Nav.Link>Marketplace</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to={`/dao`}>
+                  <Nav.Link eventKey="dao">Gyza Dao</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to={`/yourNaps`}>
+                  <Nav.Link eventKey="yourNaps">Your Naps</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+          </menu>
+        </header>
+        <div className="main-body">
+            <Routes>
+              <Route path="/" element={<MarketplacePage />} />
+              <Route path="Marketplace" element={<MarketplacePage />} />
+              <Route path="dao" element={<Dao />} />
+              <Route path="/nap/:id" element={<Nap id={p => p.id} />} />
+            </Routes>          
         </div>
-        <menu className="menu">
-          <Nav
-            activeKey="/Marketplace"
-            onSelect={(selectedKey) => {setPage(selectedKey)}}
-          >
-            <Nav.Item>
-            <Nav.Link eventKey="MarketPlace">Marketplace</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="dao">Gyza Dao</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="yourNaps">Your Naps</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </menu>
-      </header>
-      <div className="main-body">
-        {page}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MarketplacePage />} />
-            <Route path="Marketplace" element={<MarketplacePage />} />
-            <Route path=":nap" element={<MarketplacePage />} />
-          </Routes>
-        </BrowserRouter>,
-
-        
-      </div>
+      </BrowserRouter>
     </div>
   );
 }
