29bd1738c71d627d66701f6fb8d9046014d8ebdc
Zane Witherspoon <zane.witherspoon@gmail.com>
2022-02-20 00:57:01 -0700

    your naps renders

--- frontend/src/components/yourNaps.js
+++ frontend/src/components/yourNaps.js
@@ -6,35 +6,57 @@ import "../styles/marketplacePage.css";
 
 function YourNaps(props) {
   const [yourNaps, setYourNaps] = useState([]);
+  const [listItems, setListItems] = useState();
 
   useEffect(() => {
-    if(props.address && props.napsContract) {
+    if(Object.keys(props.address).length !== 0 && Object.keys(props.napsContract).length !== 0) {
       props.napsContract.methods.getUserNaps(props.address).call(
         {
           from: props.address,
-        }, (err, res) => {
+        }, async (err, res) => {
           if (!err) {
             console.log(res);
+
+            let tempNaps = []
+            for (let i = 0; i < res.length; i++) {
+              if(res[i]=="1") {
+                const nap = await new Promise((resolve, reject) => {
+                  props.napsContract.methods.getNapDetails(i).call(
+                    {
+                      from: props.address,
+                    }, (err, res) => {
+                      if (!err) {
+                        resolve({
+                          id: res[4],
+                          level: res[1],
+                          price: res[2],
+                          img: "https://ih1.redbubble.net/image.630419865.4942/bg,f8f8f8-flat,750x,075,f-pad,1000x1000,f8f8f8.jpg"
+                        })
+                      }
+                    })
+                })
+
+                tempNaps.push(nap)
+                console.log(nap)
+              }
+            }
+            console.log(tempNaps)
+            setYourNaps(tempNaps)
+
           } else {
             console.log(err);
           }
         })
-
-      console.log("yourNaps");
-      let napsFakeData = [];
-      for (let i = 0; i < 4; i++) {
-        napsFakeData.push({
-          id: i,
-          level: 3,
-          price: "0.06 ETH",
-          img: "https://ih1.redbubble.net/image.630419865.4942/bg,f8f8f8-flat,750x,075,f-pad,1000x1000,f8f8f8.jpg",
-        });
-      }
-      setYourNaps(napsFakeData);
     }
   }, [props.address]);
 
+  useEffect(() => {
+    console.log("yourNaps Updated")
+    console.log(yourNaps)
+  }, [yourNaps]);
+
   function renderCards() {
+    console.log(yourNaps)
     const listItems = yourNaps.map((d) => (
       <Card className="card" key={d.id}>
         <Card.Img
